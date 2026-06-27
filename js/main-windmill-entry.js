b2400e34dc6f84e776b49a4e3735d9f912f22374
Ashton Farmer <ashton20.bama@gmail.com>
2026-06-26 20:43:34 -0500

    Add windmill entrance script

---
+++ js/main-windmill-entry.js
@@ -0,0 +1,10 @@
+// Adds the Windmill of Wishes entrance to the main map without changing game.js.
+
+locs.push({
+  name:'Windmill of Wishes',
+  x:727,
+  y:260,
+  r:70,
+  page:'windmill.html',
+  text:'Press E to enter the Windmill of Wishes 🌾'
+});
