7ce885f68e4e317870d467e1521f1a7db85b00ef
Ashton Farmer <ashton20.bama@gmail.com>
2026-06-27 01:29:22 -0500

    Add underground entrance script

---
+++ js/main-silo-entry.js
@@ -0,0 +1,10 @@
+// Adds the underground cave entrance to the main map without changing game.js.
+
+locs.push({
+  name:'Underground Cave',
+  x:982,
+  y:240,
+  r:70,
+  page:'silo.html',
+  text:'Press E to enter the silo'
+});
